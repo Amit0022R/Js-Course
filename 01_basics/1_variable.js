c3f4b5fd014e019c31442775bff36dd61b2e60ef
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2024-08-24 09:21:41 +0000

    1_variable

---
+++ 01_basics/1_variable.js
@@ -0,0 +1,23 @@
+const accountId = 144553
+let accoundEmail = "amit@google.com"
+var accountPassword = "12345"
+accountCity = "Bhiwani"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+
+accoundEmail = "ha@hc.com"
+accountPassword = "21213"
+accountCity = "delhi"
+
+
+console.log(accountId);
+
+/*
+Prefer not to use var because of issue in block scope and functional scope 
+*/
+
+
+console.table([accountId, accoundEmail, accountPassword, accountCity, accountState]) // tabular structure mei show hoge
