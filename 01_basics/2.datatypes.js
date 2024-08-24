bbc1950c6d3b6a1ad83a4c02993dc6b922a735d0
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2024-08-24 10:45:58 +0000

    datatypes

---
+++ 01_basics/2.datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) // We are using node js not browser
+
+console.log(3 + 3)
+// code readibility should be high
+
+console.log("Amit Kumar")
+
+let name = "Amit"
+let age = 19
+let isLoggedIn = false
+let state;
+
+// Number => 2 to power 53
+// bigint 
+// string => ""
+/// boolean = > true/false
+// null => standalone value or representation of empty value
+// undefined => variable declare kiya but koi value nhi di hai variable ko
+// symbol => unique
+
+// object
+
+console.log(typeof "Amit"); // string
+console.log(typeof age); // number
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
+
+                                    
+
+
