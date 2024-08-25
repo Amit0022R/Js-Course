954e95238d372bfa6bbf03e4ccc834b2e0d13c0c
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2024-08-25 13:57:09 +0000

    conversion of datatypes

---
+++ 01_basics/3_conversionOperation.js
@@ -0,0 +1,39 @@
+// let score = 33
+// console.log(typeof score); // number
+// console.log(typeof (score)); // number
+
+let score = "33"
+
+// console.log(typeof score); // string
+// console.log(typeof (score)); // string
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); // number
+// console.log(valueInNumber); // 33
+
+// "33" => 33
+// "33abx" => NaN(not a number)
+// true => 1; false => 0
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); // true
+
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); // false
+
+// let isLoggedIn = "AMit Kumar"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); // true
+
+// 1 => true; 0 => false
+// "" => false
+// "add" => 
+    
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber); // 33
+console.log(typeof stringNumber); // string
+
