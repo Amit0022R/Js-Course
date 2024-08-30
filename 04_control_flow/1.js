31afda3d0942276487de8126dec51b1f3a174127
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2024-08-30 09:39:45 +0000

    control flow 1

---
+++ 04_control_flow/1.js
@@ -0,0 +1,30 @@
+// if 
+
+
+// if(3 == "3") { 
+//     console.log("You are right here!!!!"); // execute
+// }
+
+// type checking ke strict equal aata hai -> ===
+// if(3 === "3") { 
+//     console.log("You are right here!!!!"); // not executed
+// }
+
+// Operators => < , > , <= , >= , == , != , === , !== ,   
+
+const score = 200
+
+if (score > 100) {
+    const power = "fly"
+    console.log(`User Power: ${power}`);
+}
+// var power likne se hum power ko scope ke outside bhi access kr skte
+
+
+
+
+
+
+
+
+
