f510bff1f76b373146f508065cb0892981d59436
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2024-09-04 09:42:39 +0000

    1.js in control flow

--- 04_control_flow/1.js
+++ 04_control_flow/1.js
@@ -12,15 +12,44 @@
 
 // Operators => < , > , <= , >= , == , != , === , !== ,   
 
-const score = 200
+// const score = 200
 
-if (score > 100) {
-    const power = "fly"
-    console.log(`User Power: ${power}`);
-}
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User Power: ${power}`);
+// }
 // var power likne se hum power ko scope ke outside bhi access kr skte
+// var ka scope completely global hega
 
 
+// const balance = 1000
+
+// if (balance > 500) console.log("test");
+// this is implicit scope
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+// } else if(balance < 900) {
+//     console.log("less than 750"); 
+// } else {
+//     console.log("less than 1200");
+// }
+
+
+// const userLoggedIn = true;
+// const debitCard = true;
+// const loggedInFromGoogle = false
+// const loggedInFromEmail = true
+
+// if (userLoggedIn && debitCard ) {
+//     console.log("Allow to buy courses");
+// }
+
+// if ( loggedInFromGoogle  ||  loggedInFromEmail ) {
+//     console.log("User logged in");
+// }
 
 
 
