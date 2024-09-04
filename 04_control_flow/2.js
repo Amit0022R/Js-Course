866140b392ac081e0cecc570d501c52592d42954
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2024-09-04 10:03:32 +0000

    2.js

---
+++ 04_control_flow/2.js
@@ -0,0 +1,64 @@
+// BASIC SYNTAX
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 6
+
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("Febraury");
+//         break; 
+//     case 3:
+//         console.log("March");
+//         break; 
+//     case 4:
+//         console.log("April");
+//         break; 
+//     case 5:
+//         console.log("May");
+//         break; 
+//     case 6:
+//         console.log("June");
+//         break; 
+
+//     default:
+//         console.log("default case match");   
+//         break;
+// }
+
+
+const month = "june"
+
+switch (month) {
+    case "january":
+        console.log("1");
+        break;
+    case "febraury":
+        console.log("2");
+        break; 
+    case "march":
+        console.log("3");
+        break; 
+    case "april":
+        console.log("4");
+        break; 
+    case "may":
+        console.log("5");
+        break; 
+    case "june":
+        console.log("6");
+        break; 
+
+    default:
+        console.log("default case match");   
+        break;
+}
