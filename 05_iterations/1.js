6bd289cfa4739bdcf1f62499e96e98446c199bcd
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2024-09-10 10:09:03 +0530

    for loop with break and continue

---
+++ 05_iterations/1.js
@@ -0,0 +1,67 @@
+                // for loop
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element); 
+// }
+
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     if( element == 5 ) {
+//         console.log("5 is best number");
+        
+//     }
+//     console.log(element); 
+// }
+
+
+// for (let i = 0; i < 3; i++) {
+//     // console.log(`Outer loop value: ${i}`);
+    
+//     for (let j = 0; j < 3; j++) {
+//         // console.log(`Inner loop value: ${j} and inner loop ${i}`);
+//         // console.log(i + '*' + j + ' = ' + i*j);
+//     }
+// }
+
+
+// let myArray = ["amit", "anuj", "super"]
+// console.log(`MyArray length is ${myArray.length}`);
+
+// for (let i = 0; i < myArray.length; i++) {
+//     const element = myArray[i];
+//     console.log(element);
+     
+// }
+
+
+                   // break and continue
+
+
+// for (let i = 1; i <= 20; i++) {
+//     if(i == 5 ) {
+//         console.log(`Detected 5`);
+//         break;
+        
+//     }
+//     console.log(`Value of i is ${i}`);
+    
+// }
+
+for (let i = 1; i <= 20; i++) {
+    if(i == 5 ) {
+        console.log(`Detected 5`);
+        continue
+        
+    }
+    console.log(`Value of i is ${i}`);
+    
+}
+
+
+
+
+
+
+
