7ac17719f329edfc0ad8822823b166ad19b1dced
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2024-09-10 11:26:54 +0530

    high order array 2

---
+++ 05_iterations/4.js
@@ -0,0 +1,39 @@
+
+const myObject = {
+    js: 'javascript',
+    go: 'golang',
+    cpp: 'C++',
+    rb: 'ruby',
+    R: 'rust'
+}
+
+for (const key in myObject) {
+    // console.log(key);
+    // console.log(myObject[key]);
+    // console.log(`${key} shortcut is for ${myObject[key]}`);   
+}
+
+// const programming = ["js", "java", "cpp", "py", "rb"]
+
+// for (const key in programming) {
+//     console.log(programming[key]); 
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
