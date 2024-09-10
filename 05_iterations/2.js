cac39f1b0629df6bcd9f8a4f8916cce0dcf02207
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2024-09-10 10:21:50 +0530

    while and do while loop

---
+++ 05_iterations/2.js
@@ -0,0 +1,37 @@
+                     // while loop
+
+// let i = 0                    
+// while ( i <= 10) {
+//     console.log(`Value of i is ${i}`);
+//     i = i + 2;
+// }
+
+// let myArray = ['abc', 'def', 'ghi']
+// let arr = 0
+// while ( arr < myArray.length ) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr = arr + 1;
+// }
+
+                  // do while loop
+
+// let score = 1                  
+// do {
+//     console.log(`Score is ${score}`);
+//     score++;
+// } while (score <= 10);
+
+
+let score = 11                  
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);
+
+
+
+
+
+
+
+
