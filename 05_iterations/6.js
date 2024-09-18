c16f083474eacfff45597ee54cb28d110b4fa5a0
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2024-09-18 10:14:01 +0530

    filter map and reduce part 1

---
+++ 05_iterations/6.js
@@ -0,0 +1,59 @@
+// const coding = ["js", "py", "java", "ruby", "rust"]
+
+// for each value return nhi krne wala
+
+// const values =  coding.forEach( (item) => {
+//     console.log(item);
+//     return item
+// } )
+
+// console.log(values);
+
+// console.log(values); // undefined
+
+// filter => Returns the elements of an array that meet the condition specified in a callback function , also filter mei scope mei return use kro acha rhega
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNums.filter( (num) => num > 4 )
+// console.log(newNums); // [ 5, 6, 7, 8, 9, 10 ]
+
+// const newNums = myNums.filter( (num) => {
+//         num > 4 
+// } )
+// console.log(newNums); // []
+
+
+// const newNums = myNums.filter( (num) => {
+//    return num > 4 
+// } )
+// console.log(newNums); // [ 5, 6, 7, 8, 9, 10 ]
+
+
+// const newNums = []
+// myNums.forEach( (num) => {
+//     if( num > 4 ) {
+//         newNums.push(num)
+//     }
+// } )
+// console.log(newNums); // [ 5, 6, 7, 8, 9, 10 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
