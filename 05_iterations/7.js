33a5fc8ded73a3d1fc985c2fed94618ee6a3a99d
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2024-09-18 10:36:53 +0530

    Map In Js

--- 05_iterations/7.js
+++ 05_iterations/7.js
@@ -1,32 +1,27 @@
+// map => Calls a defined callback function on each element of an array, and returns an array that contains the results.
+
+// const myNumers = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNumers.map( (num) => num + 10 )
+// console.log(newNums);
+// [ 11, 12, 13, 14, 15,16, 17, 18, 19, 20]
+    
+// const myNumers = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNumers.map( (num) => { return num + 10 })
+// console.log(newNums);
+// [ 11, 12, 13, 14, 15,16, 17, 18, 19, 20]
+
+
 const myNumers = [1,2,3,4,5,6,7,8,9,10]
 
-myNumers.map()
+// chaining
+const newNums = myNumers
+            .map( (num) => num * 10 )
+            .map( (num) => num + 1 )
+            .filter( (num) => num >= 40 )
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(newNums); 
 
 
 
